c62f2c183673981aa46966bc316a2218a819871b
Sachith Rukshan <sachithrukshanmail@gmail.com>
2024-08-09 08:45:17 +0530

    new

--- apps/host/src/app/app.routes.ts
+++ apps/host/src/app/app.routes.ts
@@ -1,3 +1,9 @@
+import { loadRemoteModule } from '@angular-architects/native-federation';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => loadRemoteModule('layout', './Component').then(m => m.AppComponent)
+    }
+];
